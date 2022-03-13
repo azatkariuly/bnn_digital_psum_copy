656b64af6c75663f10ce15370f260df222b8a40f
azatkariuly <azat.kariuly@gmail.com>
2022-03-14 07:08:56 +0900

    initial commit

--- imagenet/main.py
+++ imagenet/main.py
@@ -326,7 +326,6 @@ def load_my_state_dict(self, state_dict):
         print(name)
 
     for name, param in state_dict.items():
-        '''
         if 'layer' in name:
             temp = name.split('layer')
             list1 = list(temp[1])
@@ -343,15 +342,14 @@ def load_my_state_dict(self, state_dict):
                 temp[1] = '.bn1.' + temp1[2]
 
             name = temp[0] + 'downsample' + temp[1]
-        '''
+
         if name not in own_state:
             continue
-        '''
+
         if isinstance(param, Parameter):
             # backwards compatibility for serialized parameters
             param = param.data
         own_state[name].copy_(param)
-        '''
 
 if __name__ == '__main__':
     main()
